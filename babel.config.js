34c9840ab3cb5f2c48a538415df24c4f25269a2f
itsajitkumar <88327747+proajitkumar@users.noreply.github.com>
2023-01-22 22:17:50 +0530

    config for vector icon

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  dependencies: {
+    'react-native-vector-icons': {
+      platforms: {
+        ios: null,
+      },
+    },
+  },
 };
